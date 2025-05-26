9cfb5c37f545e08f2b3301dcc03cc9c3766c587c
jedaqsaul <jedaqsaul@gmail.com>
2025-05-26 20:29:51 +0300

    Create some devs

--- lib/seed.py
+++ lib/seed.py
@@ -23,5 +23,13 @@ def seed_data():
     c1=Company(name='Jedaq', founding_year=2005)
     c2=Company(name='Amazon', founding_year=2010)
 
-    
+    # now let me create some devs here
+    d1=Dev(name="Alice")
+    d2=Dev(name="Aquila")
+
+    # Add companies and devs to session
+    session.add_all([c1,c2,d1,d2])
+    session.commit() #commit to generate ids for foreign keys
+
+    # create freebies liked w
 
