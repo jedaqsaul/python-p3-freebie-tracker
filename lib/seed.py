88c8d1f46b1e43bb00b3fbb626b1436616667f27
jedaqsaul <jedaqsaul@gmail.com>
2025-05-26 20:29:24 +0300

    Create some companies

--- lib/seed.py
+++ lib/seed.py
@@ -5,4 +5,23 @@ from sqlalchemy import create_engine
 
 from models import Company, Dev, Freebie
 
+# create engine and session
+
+engine=create_engine("sqlite:///freebies.db")
+Session=sessionmaker(bind=engine)
+session=Session()
+
+def seed_data():
+    # create some companies# create engine and session
+
+engine=create_engine("sqlite:///freebies.db")
+Session=sessionmaker(bind=engine)
+session=Session()
+
+def seed_data():
+    # create some companies
+    c1=Company(name='Jedaq', founding_year=2005)
+    c2=Company(name='Amazon', founding_year=2010)
+
+    
 
