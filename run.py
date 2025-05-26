0cd30f0eb8e8eaedb19ea152b51b6f7714ed76c3
jedaqsaul <jedaqsaul@gmail.com>
2025-05-26 20:55:27 +0300

    :Creates a function to list all devs

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from lib.models import Dev, Company, Freebie
 
 
 # set up DB session
@@ -9,6 +10,17 @@ from sqlalchemy import create_engine
 engine=create_engine("sqlite:///freebies.db")
 Session =sessionmaker(bind=engine)
 session=Session()
+
+
+def print_line():
+    print("_"*60)
+
+def list_devs():
+    devs=session.query(Dev).all()
+    print_line()
+    for dev in devs:
+        print(f"{dev.id}: {dev.name}")
+    print_line()
 
 
 
